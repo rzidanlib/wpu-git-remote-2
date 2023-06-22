44bfe4e3ab1f2d9487ced60aeb55516615008ff9
Zidan <library.devz@gmail.com>
2023-06-22 22:11:27 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
   <h1>Hello world!</h1>
   <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolorum molestias assumenda, vero sit quidem? Sint temporibus culpa corrupti, quo, voluptatibus aliquid saepe inventore reprehenderit nulla a recusandae perferendis dolorum?</h1>
+  <p>Paragraf</p>
 </body>
 </html>
